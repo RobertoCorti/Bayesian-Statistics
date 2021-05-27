6c941c996da01fe1a3a6a3e0cde67a1fc38de464
Roberto <robi290596@gmail.com>
2021-05-27 23:16:02 +0200

    posterior plots for model_1 + started model_2 stan

---
+++ project/model_2.stan
@@ -0,0 +1,107 @@
+data {
+  int<lower=0> N; //number of games
+  int<lower=0> T; //number of teams
+  
+  int<lower=0> y_home[N];
+  int<lower=0> y_away[N];
+  
+  int<lower=1, upper=T> home_team_index[N];
+  int<lower=1, upper=T> away_team_index[N];
+   
+}
+
+parameters {
+  real home;
+  
+  simplex[3] pi_att[T];
+  simplex[3] pi_def[T];
+  
+  real att[3];
+  real def[3];
+  
+  real<lower=0> tau_att[3];
+  real<lower=0> tau_def[3];
+  
+  vector[T] att_team_star;
+  vector[T] def_team_star;
+  
+}
+
+transformed parameters{
+   real sigma_att[3] = inv(tau_att);
+   real sigma_def[3] = inv(tau_def);
+
+   vector[T] att_team;
+   vector[T] def_team;
+   
+   for(t in 1:T){
+     att_team[t] = att_team_star[t] - mean(att_team_star);
+     def_team[t] = def_team_star[t] - mean(def_team_star);
+   }
+}
+
+
+model {
+  
+  // Prior on home effect
+  home ~ normal(0, 10000);
+  
+  //Priors on the random effects
+  // group 1: bottom-table team
+  tau_att[1] ~ gamma(0.01, 0.01);
+  tau_def[1] ~ gamma(0.01, 0.01);
+  att[1] ~ normal(0, 1000) T[-3, 0];
+  def[1] ~ normal(2, 1000) T[0, 3];
+  
+  // group 2: mid-table teams
+  tau_att[2] ~ gamma(0.01, 0.01);
+  tau_def[2] ~ gamma(0.01, 0.01);
+  att[2] ~ normal(0, sigma_att[2]);
+  def[2] ~ normal(0, sigma_def[2]);
+  
+  // group 3: top-table teams
+  tau_att[3] ~ gamma(0.01, 0.01);
+  tau_def[3] ~ gamma(0.01, 0.01);
+  att[3] ~ normal(2, 1000) T[0,3];
+  def[3] ~ normal(-2, 1000) T[-3,0];
+  
+  // Grouping effect
+  // Prior on team cateogry probability
+  for(t in 1:T){
+    pi_att[t] ~ dirichlet(rep_vector(1, 3));
+    
+    pi_def[t] ~ dirichlet(rep_vector(1, 3));
+  }
+  
+  // Model probability for team category + model for team attack/defense effect
+  for(t in 1:T){
+    real lp[3];
+    lp[1]= log(pi_att[t,1]) + student_t_lpdf( att_team_star[t] | 4, att[1], sigma_att[1]);
+    lp[2]= log(pi_att[t,2]) + student_t_lpdf( att_team_star[t] | 4, att[2], sigma_att[2]);
+    lp[3]= log(pi_att[t,3]) + student_t_lpdf( att_team_star[t] | 4, att[3], sigma_att[3]);
+    target += log_sum_exp(lp);
+    
+    lp[1]= log(pi_def[t,1]) + student_t_lpdf( def_team_star[t] | 4, def[1], sigma_def[1]);
+    lp[2]= log(pi_def[t,2]) + student_t_lpdf( def_team_star[t] | 4, def[2], sigma_def[2]);
+    lp[3]= log(pi_def[t,3]) + student_t_lpdf( def_team_star[t] | 4, def[3], sigma_def[3]);
+    target += log_sum_exp(lp);
+  }
+
+
+  // Model probability for home and away goals 
+  
+  y_home ~ poisson_log(home + att_team[home_team_index] + def_team[away_team_index]);
+  y_away ~ poisson_log(att_team[away_team_index] + def_team[home_team_index]);
+  
+}
+/*
+generated quantities {
+  int<lower=0> y_home_rep[N];
+  int<lower=0> y_away_rep[N];
+  
+  for (n in 1:N) {
+    y_home_rep[n] = poisson_log_rng(home + att_team[home_team_index[n]] + def_team[away_team_index[n]]);
+    y_away_rep[n] = poisson_log_rng(att_team[away_team_index[n]] + def_team[home_team_index[n]]);
+  }
+}
+*/
--- project/posterior_inference.R
+++ project/posterior_inference.R
@@ -1,7 +1,114 @@
+library(dplyr)
+library(bayesplot)
+
+
+# read data
 fit_model_1 <- readRDS("models/model_1_fit.rds")
+y_home_rep <- readRDS("y_home_rep.rds")
+y_away_rep <- readRDS("y_away_rep.rds")
+df <- readRDS("./data/season1991.rds")
+df_ranking <- readRDS("./data/season1991_ranking.rds")
 
 print(fit_model_1, pars = c('att_team','def_team','home'))
 
+# create ranking with simulations
+df_simulate <-  data.frame(Date = df$Date,
+                           Season = df$Season,
+                           home = df$home,
+                           visitor = df$visitor,
+                           hgoal= as.integer(round(colMeans(y_home_rep))),
+                           vgoal= as.integer(round(colMeans(y_away_rep))), 
+                           home_id = df$home_id,
+                           visitor_id = df$visitor_id)
 
-y_home_rep <- readRDS("y_home_rep.rds")
-y_away_rep <- readRDS("y_away_rep.rds")
+
+temp <-
+  rbind(
+    df_simulate %>% select(team=home, opp=visitor, GF=hgoal, GA=vgoal),
+    df_simulate %>% select(team=visitor, opp=home, GF=vgoal, GA=hgoal)
+  ) #rbind two copies of the orignal df, simply reversing home/away team for each match
+
+df_ranking_sim<-
+  temp %>%
+  mutate(GD = GF-GA) %>%
+  group_by(team) %>%
+  summarize(games_played = n(),
+            goals_made = sum(GF),
+            goals_received = sum(GA),
+            goals_difference = sum(GD),
+            wins = sum(GD>0),
+            draws = sum(GD==0),
+            losses = sum(GD<0)
+  ) %>%
+  mutate(Pts = (wins*2) + draws) %>%
+  arrange(desc(Pts))
+
+remove(temp)
+df_ranking_sim <- df_ranking_sim %>% mutate(position = as.integer(rank(desc(Pts))))
+
+# create cum points lines
+
+cum_points <- function(df){
+  
+  team_names <- sort(unique(df$home))
+  cum_points_list <- vector("list", length = length(team_names))
+  names(cum_points_list) <- team_names
+  
+  for (name in team_names) {
+    df_team <- df %>% 
+      filter(home==name | visitor==name) %>% 
+      select(Date=Date, home=home, visitor=visitor, hgoal=hgoal, vgoal=vgoal) %>% 
+      mutate(loc=ifelse(home==name, 1, 0)) %>%
+      mutate(DG =ifelse(loc==1, hgoal-vgoal, vgoal-hgoal)) %>% 
+      mutate(point=ifelse(DG>0, 2, ifelse(DG==0, 1, 0)))
+    
+    cum_points_list[[name]] <- cumsum(df_team$point)
+  }
+  
+  return(cum_points_list)
+}
+
+
+cum_points_list_sim <- cum_points(df_simulate)
+cum_points_list <- cum_points(df)
+
+# Set plot layout
+layout(mat = matrix(c(1:9), 
+                    nrow = 3, 
+                    ncol = 3),
+             heights = c(1, 1),    # Heights of the two rows
+             widths = c(1, 1))     # Widths of the two columns
+
+
+x<-1:34;
+for (team in sort(unique(df$home))[1:9]) {
+  
+  y1=cum_points_list[[team]]; y2=cum_points_list_sim[[team]]
+  plot(x,y1,xlim = c(0,34),ylim = c(0,60),type = "l", col="black", main=team, xlab="Games", ylab="Points")
+  lines(x, y2, pch=18, col="red", type="l", lwd=2)
+  legend(1, 50, legend=c("Real values", "Fitted values"),
+         col=c("black", "red"), lty=1:1, lwd=2:2)
+}
+
+# Set plot layout
+layout(mat = matrix(c(1:9), 
+                    nrow = 3, 
+                    ncol = 3),
+       heights = c(1, 1),    # Heights of the two rows
+       widths = c(1, 1))     # Widths of the two columns
+
+
+x<-1:34;
+for (team in sort(unique(df$home))[9:18]) {
+  print(team)
+  y1=cum_points_list[[team]]; y2=cum_points_list_sim[[team]]
+  plot(x,y1,xlim = c(0,34),ylim = c(0,60),type = "l", col="black", main=team, xlab="Games", ylab="Points")
+  lines(x, y2, pch=18, col="red", type="l", lwd=2)
+  legend(1, 50, legend=c("Real values", "Fitted values"),
+         col=c("black", "red"), lty=1:1, lwd=2:2)
+}
+
+
+
+
+
